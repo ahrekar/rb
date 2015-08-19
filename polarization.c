65c4246e6da6baab081197b407ec8f27cd2b5239
tranthamkw <tranthamkw@unk.edu>
2015-08-19 09:45:26 -0500

    Reverted polarization.c so that it doesn't set up the USB in a function, I ran into a lot of difficulties trying to do this, so I am abandoning it for now.
    Karl

--- polarization.c
+++ polarization.c
@@ -21,9 +21,6 @@
 #define CLK 0
 #define DIR 1
 
-int setUpUSB(HIDInterface* hid, hid_return* ret);
-int closeUSB(HIDInterface* hid, hid_return* ret);
-
 int main (int argc, char **argv)
 {
 	int counts,i,steps,nsteps,ninc,dwell,aout;
@@ -41,29 +38,36 @@ int main (int argc, char **argv)
 	__u8 input, pin = 0, channel, gain;
 
 	HIDInterface*  hid = 0x0;
-	hid_return* ret;
+	hid_return ret;
 	int interface;
 
 	// get parameters
+
 	if (argc==6){
 		nsteps=1200*atoi(argv[1]);
 		ninc=atoi(argv[2]);
 		dwell=atoi(argv[3]);
 		aout=atoi(argv[4]);
 		strcpy(comments,argv[5]);
-	} else {
-		printf("usage '~$ sudo ./polarization <num_rotations> <step_size> <pmt_dwell> <aout_for_target> <comments_in_double_quotes>'\n");
-		printf("                                                          (in seconds)                  (Specify nominal electron) '\n");
-		printf("                                                                                        (energy.                 ) '\n");
-		return 1;
+		} else {
+	printf("usage '~$ sudo ./polarization <num_rotations> <step_size> <pmt_dwell> <aout_for_target> <comments_in_double_quotes>'\n");
+	return 1;
 	}
 
 
 	// set up USB interface
-	if(!setUpUSB(hid,ret)){ // if setUpUSB was not completed successfully, end the program.
-		return 1;
+	ret = hid_init();
+	if (ret != HID_RET_SUCCESS) {
+		fprintf(stderr, "hid_init failed with return code %d\n", ret);
+		return -1;
 	}
-	printf("usb found!\n");
+
+	if ((interface = PMD_Find_Interface(&hid, 0, USB1208LS_PID)) < 0) {
+		fprintf(stderr, "USB 1208LS not found.\n");
+		exit(1);
+	} else {
+		printf("USB 208LS Device is found! interface = %d\n", interface);
+	}
 
 
 	// config mask 0x01 means all inputs
@@ -71,9 +75,9 @@ int main (int argc, char **argv)
 	usbDConfigPort_USB1208LS(hid, DIO_PORTA, DIO_DIR_OUT);
 	usbDOut_USB1208LS(hid, DIO_PORTA, 0x0);
 	usbDOut_USB1208LS(hid, DIO_PORTA, 0x0);
-	printf("usb config complete!\n");
 
 	// set up for stepmotor
+
 	wiringPiSetup();
 	pinMode(CLK,OUTPUT);
 	pinMode(DIR,OUTPUT);
@@ -84,10 +88,10 @@ int main (int argc, char **argv)
 	time(&rawtime);
 	timeinfo=localtime(&rawtime);
 	strftime(buffer,80,"/home/pi/RbData/POL%F_%H%M%S.dat",timeinfo);
-	printf("Generated filename\n");
 
 	printf("\n");
-	printf("%s\n",buffer);
+	printf(buffer);
+	printf("\n");
 
 
 	fp=fopen(buffer,"w");
@@ -95,7 +99,30 @@ int main (int argc, char **argv)
 		printf("unable to open file \n");
 		exit(1);
 	}
-	fprintf(fp,"#%s\n",buffer);
+	fprintf(fp,buffer);
+	fprintf(fp,"\n");
+	/*
+	DO AWAY WITH THIS. THIS INFORMATION IS NOT SPECIFICALLY USED IN THE PROGRAM. USER SHOULD JUST SPECIFY THE NOMINAL ELECTRON ENERGY
+	printf("Enter filament bias potential ");
+	scanf("%f",&bias);
+	printf("Enter target offset potential ");
+	scanf("%f",&offset);
+	fprintf(fp,"filament bias %4.2f\n",bias);
+	fprintf(fp,"target offset %4.2f\n",offset);
+	*/
+
+	// Set up for stepmotor
+	/*  REPLACED WITH COMMAND LINE ARGS
+	printf("Enter total number of steps (1200/revolution) ");
+	scanf("%d",&nsteps);
+	printf("Enter number of steps per data point");
+	scanf("%d",&ninc);
+	printf("Enter number of seconds of data to aquire per data point");
+	scanf("%d",&dwell);
+	printf("Enter, other, single line comments for data run(80 char limit): ");
+	scanf("%s",buffer);
+	fprintf(fp,buffer);
+	*/
 
 	// RUDAMENTARIY ERROR CHECKING
 	if (nsteps<1200) nsteps=1200;
@@ -106,20 +133,20 @@ int main (int argc, char **argv)
 	if (aout>1023) aout=1023;
 
 	HPcal=28.1/960.0;
-	fprintf(fp,"#nsteps %d\n",nsteps);
-	fprintf(fp,"#Aout %d\n",aout);
-	fprintf(fp,"#%s\n",comments); // Note: if there is an error in processing comments, check this line.
-	fprintf(fp,"#Assumed USB1208->HP3617A converstion %2.6f\n",HPcal);
+	fprintf(fp,"nsteps %d\n",nsteps);
+	fprintf(fp,"Aout %d\n",aout);
+	fprintf(fp,comments);
+	fprintf(fp,"\nAssumed USB1208->HP3617A converstion %2.6f\n",HPcal);
 
-	//Write Aout for He target here (Set the voltage applied to the laser to acheive the desired wavelength)
+
+	//write aout for he target here
 	usbAOut_USB1208LS(hid,1,aout);
 
 	//NOTE THAT THIS SETS THE FINAL ELECTRON ENERGY. THIS ALSO DEPENDS ON BIAS AND TARGET OFFSET.  AN EXCIATION FN WILL TELL THE
-	//USER WHAT OUT TO USE, OR JUST MANUALLY SET THE TARGET OFFSET FOR THE DESIRED ENERGY
+	// USER WHAT OUT TO USE, OR JUST MANUALLY SET THE TARGET OFFSET FOR THE DESIRED ENERGY
 
 	// Write the header for the data to the file.
-	fprintf(fp,"\n");	 // Leave a blank line to indicate a new section of the document
-	fprintf(fp,"steps\tCounts\tCurrent\n");
+	fprintf(fp,"\nsteps\tCounts\tCurrent\n");
 	gain=BP_5_00V;
 	channel = 0; // analog input for k617 ammeter
 
@@ -150,8 +177,9 @@ int main (int argc, char **argv)
 
 		current=0.0;
 		for (i=0;i<8;i++){
-			svalue = usbAIn_USB1208LS(hid,channel,gain);
-			current = current+volts_LS(gain,svalue);
+
+		svalue = usbAIn_USB1208LS(hid,channel,gain);
+		current = current+volts_LS(gain,svalue);
 		}
 		current = current/8.0;
 
@@ -167,46 +195,22 @@ int main (int argc, char **argv)
 
 
 	fclose(fp);
-
-	//cleanly close USB if it fails, indicate the error.
-	return closeUSB(hid,ret);
-}
-
-int setUpUSB(HIDInterface* hid, hid_return* ret){
-	printf("SetUpUSB Successfully called!\n");
-	int interface;
-	*ret = hid_init();
-	if (*ret != HID_RET_SUCCESS) {
-		fprintf(stderr, "hid_init failed with return code %d\n", *ret);
-		printf("hid_init failed with return code %d\n", *ret);
-		fflush(stdout);
-		return 0;
-	}
-	printf("First if statement passed!\n");
-
-	if ((interface = PMD_Find_Interface(&hid, 0, USB1208LS_PID)) < 0) {
-		fprintf(stderr, "USB 1208LS not found.\n");
-		printf("USB 1208LS not found.\n");
-		exit(1);
-	} else {
-		printf("USB 1208LS Device is found! interface = %d\n", interface);
-		return 1;
-	}
-}
-
-int closeUSB(HIDInterface* hid, hid_return* ret){
-	*ret = hid_close(hid);
-
-	if (*ret != HID_RET_SUCCESS) {
-		fprintf(stderr, "hid_close failed with return code %d\n", *ret);
+	//cleanly close USB
+	ret = hid_close(hid);
+	if (ret != HID_RET_SUCCESS) {
+		fprintf(stderr, "hid_close failed with return code %d\n", ret);
 		return 1;
 	}
 
 	hid_delete_HIDInterface(&hid);
-	*ret = hid_cleanup();
-	if (*ret != HID_RET_SUCCESS) {
-		fprintf(stderr, "hid_cleanup failed with return code %d\n", *ret);
+	ret = hid_cleanup();
+	if (ret != HID_RET_SUCCESS) {
+		fprintf(stderr, "hid_cleanup failed with return code %d\n", ret);
 		return 1;
 	}
 	return 0;
 }
+
+
+
+
