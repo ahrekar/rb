618f8e8119ecfe0347eb788aab9526849b6ae7c8
Karl Ahrendsen <karlahr2010@gmail.com>
2016-05-03 22:07:51 -0500

    Edited this program so it relies completely on the stepperMotorControl.h file. I think this really cleans up our usage of the stepper motor.

--- homemotor2.c
+++ homemotor2.c
@@ -16,21 +16,7 @@ compile
 
 #include <stdio.h>
 #include <wiringPi.h>
-
-//define the GPIO ports for the clock and direction TTL signals
-#define CLK0 3
-#define DIR0 4
-#define HOME0 5
-// these are the wiringPi GPIO numbers
-#define CLK1 21
-#define DIR1 26
-#define HOME1 22
-
-#define CLK2 1
-#define DIR2 0
-#define HOME2 2
-
-
+#include "stepperMotorControl.h"
 
 int main (int argc, char *argv[]){
 	int i, steps, dir;
@@ -40,79 +26,13 @@ int main (int argc, char *argv[]){
 		motor = atoi(argv[1]); // which steper motor
 	} else {
 
-	printf("Ussage:  ~$sudo ./homemotor2 <motor(0,1,2)> \n");
+	printf("Usage:  ~$sudo ./homemotor2 <motor(0,1,2)> \n");
 		motor = 3;// not part of the switch statment, so nuthing happens
 		steps=0;
 		dir=0;
 	}
-			wiringPiSetup();
 
-
-	switch (motor) {
-		case (0):   // this is the polarimeter
-			dir=0;
-			pinMode(CLK0,OUTPUT); //define port CLK to be an output
-			pinMode(DIR0,OUTPUT); //define port DIR to be an output
-			pinMode(HOME0,INPUT);
-			digitalWrite (CLK0,LOW);
-		if (digitalRead(HOME0)){ // already in home
-			digitalWrite (DIR0, dir);  // sets direction
-			printf("already in home, reversing 100 steps...");
-			fflush(stdout);
-			for (i=0;i<100;i++){
-			digitalWrite(CLK0,HIGH);
-			delayMicrosecondsHard(2000); // this delay will(should?) not allow other OS processes
-			digitalWrite (CLK0,LOW);
-			delayMicrosecondsHard(2000);
-			}
-		}
-		i=0;
-		dir = 1;
-		while (!digitalRead(HOME0)) {
-			digitalWrite (DIR0, dir);  // sets direction
-			digitalWrite(CLK0,HIGH);
-			delayMicrosecondsHard(2000); // this delay will(should?) not allow other OS processes
-			digitalWrite (CLK0,LOW);
-			delayMicrosecondsHard(2000);
-		}
-		if (digitalRead(HOME0)) printf("found home \n");
-		break;
-		case (1):
-			printf("This motor not setup for home detection\n");
-
-		break;
-		case (2):
-			dir=0;
-			pinMode(CLK2,OUTPUT); //define port CLK to be an output
-			pinMode(DIR2,OUTPUT); //define port DIR to be an output
-			pinMode(HOME2,INPUT);
-			digitalWrite (CLK2,LOW);
-		if (digitalRead(HOME2)){ // already in home
-			digitalWrite (DIR2, dir);  // sets direction
-			printf("already in home, reversing 100 steps...");
-			fflush(stdout);
-			for (i=0;i<100;i++){
-			digitalWrite(CLK2,HIGH);
-			delayMicrosecondsHard(1200); // this delay will(should?) not allow other OS processes
-			digitalWrite (CLK2,LOW);
-			delayMicrosecondsHard(1200);
-			}
-		}
-		i=0;
-		dir = 1;
-		while (!digitalRead(HOME2)) {
-			digitalWrite (DIR2, dir);  // sets direction
-			digitalWrite(CLK2,HIGH);
-			delayMicrosecondsHard(1200); // this delay will(should?) not allow other OS processes
-			digitalWrite (CLK2,LOW);
-			delayMicrosecondsHard(1200);
-		}
-		if (digitalRead(HOME2)) printf("found home \n");
-		break;
-
-	}
-
-
+	homeMotor(motor);
 
 return 0;
 }
