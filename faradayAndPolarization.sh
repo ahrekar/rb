cb664b3de8e484acb029ed167bac8586f94c4cfa
Karl Ahrendsen <karlahr2010@gmail.com>
2016-05-25 14:48:08 -0500

    Modified the script files so they check for the proper number of arguments and give you helpful suggestions of what arguments to provide.

--- faradayAndPolarization.sh
+++ faradayAndPolarization.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # Script to obtain polarization Data
 
-# set QWP for s+ light
-sudo ./setWavePlate 17
-sudo ./faradayscan2 "$1" "$2" "$3" "$4 s+ pump"
-sudo ./polarization 100 1 "$4 s+ pump"
+if [ "$#" -ne 4 ]; then
+	echo "usage: sudo ./faradayAndPolarization.sh <aout start> <aout end> <step size> <additional comments>"
+else
+	# set QWP for s+ light
+	sudo ./setWavePlate 17
+	sudo ./faradayscan2 "$1" "$2" "$3" "$4 s+ pump"
+	sudo ./polarization 100 1 "$4 s+ pump"
 
-# set QWP for s- light
-sudo ./setWavePlate 105
-sudo ./faradayscan2 "$1" "$2" "$3" "$4 s- pump"
-sudo ./polarization 100 1 "$4 s- pump"
+	# set QWP for s- light
+	sudo ./setWavePlate 105
+	sudo ./faradayscan2 "$1" "$2" "$3" "$4 s- pump"
+	sudo ./polarization 100 1 "$4 s- pump"
+fi
