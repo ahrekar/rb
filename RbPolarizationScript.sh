7cee6ba55f80c4209d7e34a6edb604476e82b991
Karl Ahrendsen <karlahr2010@gmail.com>
2016-08-10 18:10:12 -0500

    Got rid of the Pi-pump run, always similar to the no-pump run.

--- RbPolarizationScript.sh
+++ RbPolarizationScript.sh
@@ -4,45 +4,37 @@
 # ALL OF THE INFORMATION GATHERED BY THIS SCRIPT IS USED TO CALCUATE NUMBER DENSITY AND Rb POLARIZATION
 # Usage:
 #
-#		sudo ./kensScanScript.sh <aout start> <aout end> <step size> <additional comments>
+#		sudo ./RbPolarizationScript <aout start> <aout end> <step size> <RbScan start> <RbScan end> <additional comments>
 
-if [ "$#" -ne 4 ]; then
-	echo "usage: sudo ./RbPolarizationScript.sh <aout start> <aout end> <step size> <additional comments>"
+if [ "$#" -ne 6 ]; then
+	echo "usage: sudo ./RbPolarizationScript.sh <aout start> <aout end> <step size> <RbScan start> <RbScan end> <additional comments>"
 else
-
 	# Unblock the beam for sure
-	sudo ./setLaserFlag 0
-
-	# set QWP for s+ light
-	sudo ./homeWavePlate
+	sudo /home/pi/RbControl/setLaserFlag 1 0
 
 	# Take a picture of the chamber
 	DATE=$(date +'%Y-%m-%d')
 	FILENAME=$(date +'%Y-%m-%d_%H%M%S').jpg
-	mkdir -p /home/pi/RbPictures/$DATE
+#	mkdir -p /home/pi/RbPictures/$DATE
 	# take Picture takes the filename and a subfolder to store the 
 	# file in within the RbPicture folder
-	ssh pi@irpi "/home/pi/karlCode/takePicture.sh $FILENAME $DATE"
+#	ssh pi@irpi "/home/pi/karlCode/takeRemotePicture.sh $FILENAME $DATE"
 
-	sudo ./setWavePlate 17
-	sudo ./faradayscan2 "$1" "$2" "$3" "$4 s+ pump"
+	# set QWP for s+ light
+	sudo /home/pi/RbControl/setWavePlate 17
+	sudo /home/pi/RbControl/faradayscan "$1" "$2" "$3" "$6, s+ pump"
 
 	# set QWP for s- light
-	sudo ./setWavePlate 105
-	sudo ./faradayscan2 "$1" "$2" "$3" "$4 s- pump"
-
-	# set QWP for pi light
-	sudo ./setWavePlate 61
-	sudo ./faradayscan2 "$1" "$2" "$3" "$4 pi pump"
+	sudo /home/pi/RbControl/setWavePlate 105
+	sudo /home/pi/RbControl/faradayscan "$1" "$2" "$3" "$6, s- pump"
 
 	#set laser flag to block the pump beam
-	sudo ./setLaserFlag 3
-	sudo ./faradayscan2 "$1" "$2" "$3" "$4 no beam"
-
-	# set s laser flag to pass the beam
-	sudo ./setLaserFlag 0
+	sudo /home/pi/RbControl/setLaserFlag 1 1
+	sudo /home/pi/RbControl/faradayscan "$1" "$2" "$3" "$6, no beam"
 
 	# Absorption Scan
-	sudo ./RbAbsorbScan 675 975 5 "$4"
+	sudo /home/pi/RbControl/RbAbsorbScan "$4" "$5" 5 "$6"
 
+	# Unblock the beam
+	sudo /home/pi/RbControl/setLaserFlag 1 0
 fi
