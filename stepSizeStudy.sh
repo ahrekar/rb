756225a1cef95f0a74f8fda42b14309f14107f9d
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2017-06-17 12:01:56 -0500

    stepSizeStudy.sh with a huge run of 30 runs per variable combination.

--- stepSizeStudy.sh
+++ stepSizeStudy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for j in $(seq 1 4); do 
+for j in $(seq 1 7); do 
     for i in 7 25 35; do 
-        for k in $(seq 1 5);do
+        for k in $(seq 1 30);do
             $RBC/faradayRotation 0 0 0 "$j" "$i" "Testing how accurate the angle measured with given dataset size. Rev: $j, StepSize: $i, Run $k"
         done
     done
