e3ff93756b9cc73cc012fe5975a03ac2f2c7ec9e
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2017-08-06 16:04:34 -0500

    Changed standard deviation to report the standard deviation of the data, rather than the standard deviation of the mean.

--- mathTools.c
+++ mathTools.c
@@ -18,6 +18,27 @@ float stdDeviation(float* value, int numValues){
 	for(i=0; i < numValues;i++){
 		sum += pow(avg - value[i],2);
 	}
+	stdDev = sqrt(sum/(numValues));
+
+	return stdDev;
+}
+
+float stdDeviationMean(float* value, int numValues){
+	float stdDev, sum, avg;
+
+	// First Calculate the Average value
+	sum = 0.0;
+	int i;
+	for(i=0; i < numValues;i++){ 
+		sum += value[i];
+	}
+	avg = sum / (float) numValues;
+
+	// Then calculate the Standard Deviation of the mean
+	sum = 0.0;
+	for(i=0; i < numValues;i++){
+		sum += pow(avg - value[i],2);
+	}
 	stdDev = sqrt(sum/(numValues*(numValues-1)));
 
 	return stdDev;
