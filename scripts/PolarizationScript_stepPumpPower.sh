47e8a8479d6d315ed1064730363f9ec49e9bce88
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2019-01-12 16:04:19 -0600

    Changing variables. Need to adjust code to load in parameters a la the waveplate.

--- scripts/PolarizationScript_stepPumpPower.sh
+++ scripts/PolarizationScript_stepPumpPower.sh
@@ -4,14 +4,14 @@ if [ "$#" -ne 1 ]; then
 	echo "usage: sudo ./PolarizationScript.sh <additional comments>" 
 else
     RBC="/home/pi/RbControl"
-	FILBIAS="-130.0"
-	N2OFFSET="8.4"
-	N2SWEEP="12.2"
-	HEOFFSET=-74.3
-	CURRENTSCALE=7
-	SCANRANGE=31
-	STEPSIZE=24
-	DWELL=2
+	FILBIAS="-129.4"
+	N2OFFSET="60.0"
+	N2SWEEP="20.9"
+	HEOFFSET=-20
+	CURRENTSCALE=6
+	SCANRANGE=63
+	STEPSIZE=16
+	DWELL=1
 	NUMRUN=7
 	COMMENTS=$1
 
@@ -21,14 +21,16 @@ else
     BLOCKED=1
     UNBLOCKED=0
 
-	for power in 2500 2000 1500 1000 0; do 
+	for power in 3500 2500 1500 1250; do 
 		echo "About to change current to $power, giving 1 minutes opportunity to cancel" 
 		echo "About to change current to $power, giving 1 minute to cancel." | mutt -s "RbPi Report" karl@huskers.unl.edu
 		sleep 60
+		#sleep 3
 
 		sudo $RBC/setTACurrent $power
 		echo "Giving 30 s for the laser to settle"
   	 	sleep 30
+  	 	#sleep 3
 
 		sudo $RBC/scripts/PolarizationScript.sh $FILBIAS $N2OFFSET $N2SWEEP $HEOFFSET $CURRENTSCALE $DWELL $NUMRUN "power=$power,  $COMMENTS"
 	# power LOOP DONE
