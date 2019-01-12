6c291a28a5071bbb441c3327bb905cc094554c49
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2019-01-12 16:39:38 -0600

    Added functions for generating file names since it's a fairly involved process that is done everytime a new data file type is generated

--- fileTools.c
+++ fileTools.c
@@ -2,7 +2,10 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/stat.h> // Contains permission bits such as: S_IRWXU
 #include "fileTools.h"
+
+#define BUFSIZE 1024
 
 int append(char* _head, char* _tail)
 {
@@ -47,8 +50,8 @@ int getCommentLineFromFile(char* inputFile, char* commentDescText, char* returnS
 		exit(1);
 	}
 	do{
-		fgets(returnString,1024,data);
-        printf("Searching for comment line...\n");
+		fgets(returnString,BUFSIZE,data);
+        //printf("Searching for comment line...\n");
 	} while(strncmp(returnString,commentDescText,strlen(commentDescText)));
 	pointer = strtok(returnString,"\t");
 	pointer = strtok(NULL,"\t");
@@ -56,6 +59,46 @@ int getCommentLineFromFile(char* inputFile, char* commentDescText, char* returnS
 	returnString[strcspn(returnString,"\n")]=0;
 
 	return 0;
+}
+
+int createTimeStampedFileWithID(char* returnFilename,char* fileID){
+    time_t rawtime;
+    struct tm * timeinfo;
+    struct stat st = {0};
+    char dateString[BUFSIZE];
+    char timeString[BUFSIZE];
+    char temp[BUFSIZE];
+
+    // Get file name.  Use format "fileID"+$DATE+$TIME+".dat"
+    time(&rawtime);
+    timeinfo=localtime(&rawtime);
+    strftime(temp,BUFSIZE,"/home/pi/RbData/%F",timeinfo); //INCLUDE
+    if (stat(temp, &st) == -1){
+        mkdir(temp,S_IRWXU | S_IRWXG | S_IRWXO );
+    }
+    strftime(dateString,BUFSIZE,"%F",timeinfo); 
+    strftime(timeString,BUFSIZE,"%H%M%S",timeinfo);
+    sprintf(returnFilename,BUFSIZE,"/home/pi/RbData/%s/%s%s_%s.dat",dateString,fileID,dateString,timeString);
+}
+
+int createSummaryFileWithID(char* returnFilename,char* fileID){
+    time_t rawtime;
+    struct tm * timeinfo;
+    struct stat st = {0};
+    char dateString[BUFSIZE];
+    char timeString[BUFSIZE];
+    char temp[BUFSIZE];
+
+    // Get file name.  Use format "fileID"+$DATE+$TIME+".dat"
+    time(&rawtime);
+    timeinfo=localtime(&rawtime);
+    strftime(temp,BUFSIZE,"/home/pi/RbData/%F",timeinfo); //INCLUDE
+    if (stat(temp, &st) == -1){
+        mkdir(temp,S_IRWXU | S_IRWXG | S_IRWXO );
+    }
+    strftime(dateString,BUFSIZE,"%F",timeinfo); 
+    strftime(timeString,BUFSIZE,"%H%M%S",timeinfo);
+    sprintf(returnFilename,BUFSIZE,"/home/pi/RbData/%s/%s%s.dat",dateString,fileID,dateString);
 }
 
 int getLineNumberForComment(char* inputFile, char* commentDescText){
