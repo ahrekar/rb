e514d607a571f00634316dd970bb8ac36a5792e2
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2019-01-12 16:27:36 -0600

    Added in one more test to the mix.

--- scripts/BPDEvaluation.sh
+++ scripts/BPDEvaluation.sh
@@ -11,3 +11,5 @@ $RBC/setProbeDetuning -6
 $RBC/faradayRotation "-6 GHz Detuning, BPD measurement, $COMMENTS"
 
 $RBC/faradayScanBPD 0 117 1 "Full Scan, BPD measurement, $COMMENTS"
+
+$RBC/faradayScan 0 117 1 "Full Scan, rotating HWP, $COMMENTS"
