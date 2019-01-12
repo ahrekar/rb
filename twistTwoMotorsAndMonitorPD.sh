a9e674931c855c891cc082fac4e29ea92d0438ae
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2019-01-12 15:50:10 -0600

    Switched script to user inputting arguments rather than hard coded so that the script won't need to be committed and updated each time I use the program.

--- twistTwoMotorsAndMonitorPD.sh
+++ twistTwoMotorsAndMonitorPD.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-MIN=70
-MAX=84
-STEP=1
-RBC=/home/pi/RbControl
+if [ "$#" -ne 3 ]; then
+	echo "usage: ./twistTwoMotorsAndMonitorPD.sh <MIN STEP POS> <MAX STEP POS> <STEP SIZE>"
+else
+	MIN=$1
+	MAX=$2
+	STEP=$3
+	RBC=/home/pi/RbControl
 
-for pos in $(seq $MIN $STEP $MAX); do
-	echo "Running for QWP pos = $pos out of $MAX"
-	echo "Pausing for 5 sec to give chance to cancel"
-	sleep 5
-	$RBC/setWavePlate $pos
-	$RBC/faradayRotation "Evaluating pump polarization vs. QWP position, pos=$pos"
-done 
+	for pos in $(seq $MIN $STEP $MAX); do
+		echo "Running for QWP pos = $pos out of $MAX"
+		echo "Pausing for 5 sec to give chance to cancel"
+		sleep 5
+		$RBC/setWavePlate $pos
+		$RBC/getAngleBPD
+		#$RBC/faradayRotation "Evaluating pump polarization vs. QWP position, pos=$pos"
+	done 
