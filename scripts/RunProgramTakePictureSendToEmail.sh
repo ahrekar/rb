e63a3268af98cd23496be483f313b62f7dd11887
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2019-02-01 20:24:36 -0600

    Whatever

--- scripts/RunProgramTakePictureSendToEmail.sh
+++ scripts/RunProgramTakePictureSendToEmail.sh
@@ -9,14 +9,17 @@ UNBLOCKED=0
 BLOCKED=1
 
 
-$RBC/setLaserFlag $PROBE $UNBLOCKED
-$RBC/setLaserFlag $PUMP $UNBLOCKED
-$RBC/scripts/takePicture.sh "RbAutoPic"
+#$RBC/setLaserFlag $PROBE $UNBLOCKED
+#$RBC/setLaserFlag $PUMP $UNBLOCKED
+#$RBC/scripts/takePicture.sh "RbAutoPic"
 
 $RBC/setLaserFlag $PROBE $UNBLOCKED
+$RBC/setLaserFlag $PUMP $UNBLOCKED
+$RBC/faradayScan "Laser On, warming cell"
 $RBC/setLaserFlag $PUMP $BLOCKED
-$RBC/RbAbsorbScan 37 63 .3 "RbAutoScan"
+$RBC/faradayScan "Laser Off, warming cell"
+#$RBC/RbAbsorbScan 33 67 .3 "Number density settles."
 
 $RBC/setLaserFlag $PROBE $UNBLOCKED
 $RBC/setLaserFlag $PUMP $UNBLOCKED
-$RBC/scripts/sendLastDataImageAndPhotoToEmail.sh
+#$RBC/scripts/sendLastDataImageToEmail.sh
