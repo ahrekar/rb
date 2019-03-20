4d5fdb12fe0ab60cbd1fa71c1e4abdd5b5b8edcc
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2019-03-20 11:28:48 -0500

    Minor corrections

--- scripts/PumpCircularPumpAnalysis.sh
+++ scripts/PumpCircularPumpAnalysis.sh
@@ -8,22 +8,22 @@ UNBLOCKED=0
 RBC=/home/pi/RbControl
 
 echo "setting the probe laser to a proper detuning"
-sudo $RBC/setProbeLaser 25
+sudo $RBC/setProbeDetuning 10
 
-echo "setting the flip mirror to monitor probe beam"
-sudo $RBC/setMirror $PROBE
+echo "setting the flip mirror to monitor pump beam"
+sudo $RBC/toggleFlipMirror
 
 #set laser flag to unblock the probe beam.
 echo "Unblocking probe Beam."
 $RBC/setLaserFlag $PROBE $UNBLOCKED
 
 #set laser flag to unblock the pump beam.
-echo "Blocking Pump Beam."
+echo "unBlocking Pump Beam."
 $RBC/setLaserFlag $PUMP $UNBLOCKED
 
-for i in $(seq 0 3 349); do 
+for i in $(seq 169 1 172); do 
 	echo "Waiting for 5 seconds before starting next run..."
 	sleep 5
 	$RBC/setWavePlate $i
-    $RBC/faradayRotation "Wave plate in position $i, finding max polarization"
+    sudo $RBC/faradayRotation "Wave plate in position $i, finding max polarization"
 done
