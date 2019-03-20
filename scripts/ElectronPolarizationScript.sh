6a8636f1241b715eaa8bb1252aab8c3de645d0b6
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2019-03-20 11:20:09 -0500

    Since P_e measurements need multiple runs for error, changed the script so that it will do multiple runs by default

--- scripts/ElectronPolarizationScript.sh
+++ scripts/ElectronPolarizationScript.sh
@@ -6,40 +6,54 @@
 #
 
 if [ "$#" -ne 4 ]; then
-	echo "usage: ./ElectronPolarizationScript.sh <aout energy> <dwell> <ammeter scale> <additional comments>"
+	echo "usage: ./ElectronPolarizationScript.sh"
+	echo "				<aout energy>" 
+	echo "				<dwell>" 
+	echo "				<ammeter scale>" 
+	echo "				<additional comments>"
 else
     AOUT=$1
     DWELL=$2
 	AMMETERSCALE=$3
     COMMENTS=$4
+	DETUNING=2.75
 	LEAKCURRENT=0
 
 	source LoadWaveplatePositions.sh
 
-	echo "Blocking probe beam..."
-	sudo $RBC/setLaserFlag $PROBE $BLOCKED
+	NUMRUN=10
 
-	echo "Blocking pump beam..."
-	sudo $RBC/setLaserFlag $PUMP $BLOCKED
+	for i in $( seq 1 $NUMRUN ); do 
+		sudo $RBC/setPumpDetuning 2.75
+		echo "About to start next sequence of runs..."
+		sleep 5
+		for a in $AOUT; do 
+			echo "Blocking probe beam..."
+			sudo $RBC/setLaserFlag $PROBE $BLOCKED
 
-	echo "No pump..."
-	sudo $RBC/polarization "$AOUT" "$DWELL" "$AMMETERSCALE" "$LEAKCURRENT" "$COMMENTS, AOUT=$AOUT, pump=none"
+			echo "Blocking pump beam..."
+			sudo $RBC/setLaserFlag $PUMP $BLOCKED
 
-	echo "Unblocking pump beam..."
-	sudo $RBC/setLaserFlag $PUMP $UNBLOCKED
+			echo "No pump..."
+			sudo $RBC/polarization "$a" "$DWELL" "$AMMETERSCALE" "$LEAKCURRENT" "$COMMENTS, AOUT->$a, pump->none, Run $i/$NUMRUN"
 
-##	echo "Setting pump to Pi..."
-##	sudo $RBC/setWavePlate $PIPOS
-##	sudo $RBC/polarization "$AOUT" "$DWELL" "$AMMETERSCALE" "$LEAKCURRENT" "$COMMENTS, AOUT=$AOUT, pump=pi"
+			echo "Unblocking pump beam..."
+			sudo $RBC/setLaserFlag $PUMP $UNBLOCKED
 
-	echo "Setting pump to S+..."
-	sudo $RBC/setWavePlate $SPLUSPOS
-	sudo $RBC/polarization "$AOUT" "$DWELL" "$AMMETERSCALE" "$LEAKCURRENT" "$COMMENTS, AOUT=$AOUT, pump=s+"
+		##	echo "Setting pump to Pi..."
+		##	sudo $RBC/setWavePlate $PIPOS
+		##	sudo $RBC/polarization "$AOUT" "$DWELL" "$AMMETERSCALE" "$LEAKCURRENT" "$COMMENTS, AOUT=$AOUT, pump=pi"
 
-	echo "Setting pump to S-..."
-	sudo $RBC/setWavePlate $SMINUSPOS
-	sudo $RBC/polarization "$AOUT" "$DWELL" "$AMMETERSCALE" "$LEAKCURRENT" "$COMMENTS, AOUT=$AOUT, pump=s-"
+			echo "Setting pump to S+..."
+			sudo $RBC/setWavePlate $SPLUSPOS
+			sudo $RBC/polarization "$a" "$DWELL" "$AMMETERSCALE" "$LEAKCURRENT" "$COMMENTS, AOUT->$a, pump->s+, Run $i/$NUMRUN"
 
-	echo "Unblocking probe beam..."
-	sudo $RBC/setLaserFlag $PROBE $UNBLOCKED
+			echo "Setting pump to S-..."
+			sudo $RBC/setWavePlate $SMINUSPOS
+			sudo $RBC/polarization "$a" "$DWELL" "$AMMETERSCALE" "$LEAKCURRENT" "$COMMENTS, AOUT->$a, pump->s-, Run $i/$NUMRUN"
+
+			echo "Unblocking probe beam..."
+			sudo $RBC/setLaserFlag $PROBE $UNBLOCKED
+		done 
+	done
 fi
