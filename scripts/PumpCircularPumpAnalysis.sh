2fdfff68d6a36567f581385eb3f6b5068b0d07af
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2021-09-25 09:15:40 -0500

    Added in laser checkups

--- scripts/PumpCircularPumpAnalysis.sh
+++ scripts/PumpCircularPumpAnalysis.sh
@@ -8,10 +8,10 @@ UNBLOCKED=0
 RBC=/home/pi/RbControl
 
 echo "setting the probe laser to a proper detuning"
-sudo $RBC/setProbeDetuning -15
+sudo $RBC/setProbeDetuning -30
 
-echo "setting the probe laser to a proper detuning"
-sudo $RBC/setPumpDetuning 1.5
+echo "setting the pump laser to a proper detuning"
+sudo $RBC/setPumpDetuning -1.5
 
 echo "setting the flip mirror to monitor pump beam"
 sudo $RBC/toggleFlipMirror
@@ -24,9 +24,24 @@ $RBC/setLaserFlag $PROBE $UNBLOCKED
 echo "unBlocking Pump Beam."
 $RBC/setLaserFlag $PUMP $UNBLOCKED
 
-for i in $(seq 159 1 165); do 
+for i in $(seq 150 1 170); do 
+	echo "setting the pump laser to a proper detuning"
+	sudo $RBC/setPumpDetuning -1.5
+	echo "setting the probe laser to a proper detuning"
+	sudo $RBC/setProbeDetuning -30
 	echo "Waiting for 5 seconds before starting next run..."
 	sleep 5
 	$RBC/setWavePlate $i
-    sudo $RBC/faradayRotation "Wave plate in position $i, finding max polarization"
+    sudo $RBC/faradayRotation "wavePlatePos->$i, searching for polarization reversal"
+done
+
+for i in $(seq 62 1 82); do 
+	echo "setting the pump laser to a proper detuning"
+	sudo $RBC/setPumpDetuning -1.5
+	echo "setting the probe laser to a proper detuning"
+	sudo $RBC/setProbeDetuning -30
+	echo "Waiting for 5 seconds before starting next run..."
+	sleep 5
+	$RBC/setWavePlate $i
+    sudo $RBC/faradayRotation "wavePlatePos->$i, searching for polarization reversal"
 done
