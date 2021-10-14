e8ea49e6d232187883a33845fd11e42fbc405670
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2021-10-13 21:30:28 -0500

    This beautiful thing lets me sleep a bit longer and collect more data.

---
+++ interfacing/controlledWarmup.c
@@ -0,0 +1,63 @@
+/*
+   program to set the control temperature of the Omega CN7500
+
+   usage $ sudo ./setOmega <float temperature>
+
+   e.g  $sudo ./setOmega 45.5
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "interfacing.h"
+
+
+int main (int argc, char* argv[]){
+
+    // Variables for recording the time. 
+	time_t rawtime;
+	struct tm * timeinfo;
+
+	float returnRes, returnTarg, returnTargSet;
+	float modTemperature=10;
+	float stopPoint, ccSP;
+
+	char stringBuff[BUFSIZE];
+
+	time(&rawtime);
+	timeinfo=localtime(&rawtime);
+	strftime(stringBuff,BUFSIZE,"%F %H:%M:%S",timeinfo);
+	printf("%s\n===\n",stringBuff);
+
+	initializeBoard();
+
+
+	getPVCN7500(CN_RESERVE,&returnRes);
+	getPVCN7500(CN_TARGET,&returnTarg);
+	getSVCN7500(CN_TARGET,&returnTargSet);
+	if (returnRes == 0  || returnRes > 170){ 
+		getPVCN7500(CN_RESERVE,&returnRes);
+		getPVCN7500(CN_TARGET,&returnTarg);
+		getSVCN7500(CN_TARGET,&returnTargSet);
+	}
+
+	ccSP=200.0;
+
+	stopPoint=159.0;
+	if (returnRes < (stopPoint - modTemperature)){
+		modTemperature=5;
+		printf("temperature %3.1f < %3.1f, setting to %3.1f and %3.1f\n",returnRes, stopPoint , ccSP, returnRes + modTemperature);
+	} else if((returnRes < (stopPoint - modTemperature)) && (returnRes < stopPoint)){
+		modTemperature = stopPoint - returnRes;
+	}else {
+		modTemperature = stopPoint - returnRes;
+	}
+
+	setSVCN7500(CN_RESERVE, returnRes + modTemperature);
+	setSVCN7500(CN_TARGET, ccSP);
+
+	printf("Target Temp:%f\n",returnRes);
+
+	return 0 ;
+}
