55bb0ca619a15b29162f1660765b08c46db8c1a1
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2022-01-27 11:44:07 -0600

    formatting changes

--- interfacing/K6485meter.h
+++ interfacing/K6485meter.h
@@ -10,7 +10,6 @@
 #define K6485METERVERT 0x02
 #define K6485METERHORIZ 0x01
 
-
 int getReadingK6485(float* amps, char gpibaddress, unsigned short RS485Address);
 int getStatusK6485(unsigned char *returndata, char gpibaddress, unsigned short RS485Address);
 
